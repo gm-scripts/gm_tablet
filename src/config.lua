b65396ffc9cfd6cd508826fbca6c939f75810166
Eichenholz <62652381+Eichenholz@users.noreply.github.com>
2020-12-29 13:13:07 +0100

    Lua

---
+++ src/config.lua
@@ -0,0 +1,9 @@
+Config = {}
+
+Config.Locale = 'en'
+
+-- If true Player needs to have the tablet item to use it / If false everyone cann call it with a Hotkey
+Config.EnableItem = false
+
+-- Key to open the Tablet if "EnableItem" is set to false
+Config.OpenKey = 
