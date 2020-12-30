15a325fef4e2b341eba74cc41ffa8b26cc1a1642
obsidianical <68819302+obsidianical@users.noreply.github.com>
2020-12-30 11:25:46 +0100

    much of the code made to vue
    a lot of the code now is on vue, next the display code will be made to vue code

--- src/tablet_vue/src/App.vue
+++ src/tablet_vue/src/App.vue
@@ -1,27 +1,31 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div id="container">
+    <Frame></Frame>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import Frame from "./components/frame.vue";
 
 export default defineComponent({
   name: "App",
   components: {
-    HelloWorld
+    Frame
   }
 });
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import "./assets/scss/mixins";
+
+body,
+html {
+  background-color: #333333;
+  @include no-margin;
+  display: grid;
+  place-items: center;
+  height: 100vh;
+  width: 100vw;
 }
 </style>
