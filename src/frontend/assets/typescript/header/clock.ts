74e381e9a1d3dd2490a602fee3678a57dfe9c623
obsidianical <68819302+obsidianical@users.noreply.github.com>
2020-12-30 10:24:54 +0100

    redoing frontend with vue

--- src/frontend/assets/typescript/header/clock.ts
+++ src/frontend/assets/typescript/header/clock.ts
@@ -3,5 +3,5 @@ export default function updateClock() {
   let date = new Date();
   let mins = date.getMinutes();
   let hours = date.getHours();
-  $("#clock").text(`${hours >= 9 ? hours : `0${hours}`}:${mins >= 9 ? mins : `0${mins}`}`)
+  $("#clock").text(`${hours >= 10 ? hours : `0${hours}`}:${mins >= 10 ? mins : `0${mins}`}`)
 }
